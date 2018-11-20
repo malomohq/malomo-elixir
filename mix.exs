ef9614edb8e98b6d361299e9c2a3b528b432270e
Anthony Smith <420061+anthonator@users.noreply.github.com>
2018-11-20 16:16:12 -0500

    Added hackney to plt apps list

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Malomo.MixProject do
       version: "0.0.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [plt_add_apps: [:hackney]]
     ]
   end
 
