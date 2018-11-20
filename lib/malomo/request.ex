066d5c2b60395aa334ff327b9744d9718a949c32
Anthony Smith <420061+anthonator@users.noreply.github.com>
2018-11-20 16:04:01 -0500

    Fix typespec

--- lib/malomo/request.ex
+++ lib/malomo/request.ex
@@ -3,12 +3,12 @@ defmodule Malomo.Request do
   Represents an HTTP request.
   """
 
-  @type %__MODULE__{
-          body: binary,
-          headers: [{binary, binary}],
-          method: atom,
-          url: binary
-        }
+  @type t :: %__MODULE__{
+               body: binary,
+               headers: [{binary, binary}],
+               method: atom,
+               url: binary
+             }
 
   defstruct body: "", headers: [], method: nil, url: nil
 end
