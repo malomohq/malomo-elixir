aa047ec85f29a361d3ad6206af3cc25201546ca3
Anthony Smith <420061+anthonator@users.noreply.github.com>
2018-12-04 13:48:18 -0500

    Added package info

--- mix.exs
+++ mix.exs
@@ -8,7 +8,8 @@ defmodule Malomo.MixProject do
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      dialyzer: [plt_add_apps: [:hackney]]
+      dialyzer: [plt_add_apps: [:hackney]],
+      package: package()
     ]
   end
 
@@ -25,5 +26,16 @@ defmodule Malomo.MixProject do
       { :dialyxir, "~> 1.0-rc", only: [:dev, :test], runtime: false },
       { :ex_doc,   "~> 0.19",   only: :dev, runtime: false }
     ]
+  end
+
+  defp package do
+    %{
+      description: "Elixir client for the Malomo REST API",
+      maintainers: ["Anthony Smith"],
+      licenses: ["MIT"],
+      links: %{
+        GitHub: "https://github.com/malomohq/malomo-elixir"
+      }
+    }
   end
 end
