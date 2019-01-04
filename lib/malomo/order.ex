41307338eb796f9af4a2ff6bf5ae81c8fb3e71b3
Kurt Friedrich <kurtfriedrich@Kurts-Malomo-MacBook-Pro.local>
2019-01-04 12:36:30 -0500

    Change create orders operation to use updated server endpoint

--- lib/malomo/order.ex
+++ lib/malomo/order.ex
@@ -1,10 +1,10 @@
 defmodule Malomo.Order do
-  @spec create(binary, map) :: Malomo.Operation.t()
-  def create(customer_id, params) do
+  @spec create(map) :: Malomo.Operation.t()
+  def create(params) do
     %Malomo.Operation{
       action: :post,
       params: params,
-      path: "/customers/#{customer_id}/orders"
+      path: "/orders"
     }
   end
 end
