5269e9fc1349d92d987d27a607edf35c5c15db74
Kurt Friedrich <kurtfriedrich@Kurts-Malomo-MacBook-Pro.local>
2019-01-10 18:17:42 -0500

    Add typespec and documentation for request/2

--- lib/malomo.ex
+++ lib/malomo.ex
@@ -1,4 +1,10 @@
 defmodule Malomo do
+  @type response_t :: { :ok, Malomo.Response.t() } | { :error, any }
+
+  @doc """
+  Make a request to the Malomo API
+  """
+  @spec request(Malomo.Operation.t(), map) :: response_t
   def request(operation, config \\ %{}) do
     config = Malomo.Config.new(config)
 
