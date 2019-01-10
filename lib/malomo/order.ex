1f2611f6261b243f8e1a068c4d8324dca3704803
Kurt Friedrich <kurtfriedrich@Kurts-Malomo-MacBook-Pro.local>
2019-01-10 16:56:24 -0500

    Add remaining CRUD actions for orders

--- lib/malomo/order.ex
+++ lib/malomo/order.ex
@@ -7,4 +7,29 @@ defmodule Malomo.Order do
       path: "/orders"
     }
   end
+
+  @spec delete(binary) :: Malomo.Operation.t()
+  def delete(id) do
+    %Malomo.Operation{
+      action: :delete,
+      path: "/orders/#{id}"
+    }
+  end
+
+  @spec find(binary) :: Malomo.Operation.t()
+  def find(id) do
+    %Malomo.Operation{
+      action: :get,
+      path: "/orders/#{id}"
+    }
+  end
+
+  @spec update(binary, map) :: Malomo.Operation.t()
+  def update(id, params) do
+    %Malomo.Operation{
+      action: :put,
+      params: params,
+      path: "/orders/#{id}"
+    }
+  end
 end
