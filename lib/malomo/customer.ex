66f3ae3ac9faa2f8d1100f667369a5ba91cf3909
Kurt Friedrich <kurtfriedrich@Kurts-Malomo-MacBook-Pro.local>
2019-01-10 16:56:14 -0500

    Add remaining CRUD actions for customers

--- lib/malomo/customer.ex
+++ lib/malomo/customer.ex
@@ -7,4 +7,29 @@ defmodule Malomo.Customer do
       path: "/customers"
     }
   end
+
+  @spec delete(binary) :: Malomo.Operation.t()
+  def delete(id) do
+    %Malomo.Operation{
+      action: :delete,
+      path: "/customers/#{id}"
+    }
+  end
+
+  @spec find(binary) :: Malomo.Operation.t()
+  def find(id) do
+    %Malomo.Operation{
+      action: :get,
+      path: "/customers/#{id}"
+    }
+  end
+
+  @spec update(binary, map) :: Malomo.Operation.t()
+  def update(id, params) do
+    %Malomo.Operation{
+      action: :put,
+      params: params,
+      path: "/customers/#{id}"
+    }
+  end
 end
