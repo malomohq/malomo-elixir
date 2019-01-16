6c938385a287666a7a86148bfc5bc19ba7f83305
Kurt Friedrich <kurtfriedrich@KurtsMalomoMBP.attlocal.net>
2019-01-16 18:05:34 -0500

    Add order list function

--- lib/malomo/order.ex
+++ lib/malomo/order.ex
@@ -32,4 +32,16 @@ defmodule Malomo.Order do
       path: "/orders/#{id}"
     }
   end
+
+  @doc """
+  Retrieve a list of orders.
+  """
+  @spec list(map) :: Malomo.Operation.t()
+  def list(params \\ %{}) do
+    %Malomo.Operation{
+      action: :get,
+      params: params,
+      path: "/orders"
+    }
+  end
 end
