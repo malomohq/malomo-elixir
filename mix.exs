db45b42e901ba676bd3b98bd6723873f99355515
Anthony Smith <420061+anthonator@users.noreply.github.com>
2019-02-07 12:45:18 -0500

    Bump version to 0.0.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Malomo.MixProject do
   def project do
     [
       app: :malomo,
-      version: "0.0.3",
+      version: "0.0.4",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
