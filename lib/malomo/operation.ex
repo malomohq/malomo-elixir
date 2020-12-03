73f2672968cf1d2c4fb249eb1c2aa2a3c902a350
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-12-03 15:58:30 -0500

    Bye Felicia!

--- lib/malomo/operation.ex
+++
@@ -1,9 +0,0 @@
-defmodule Malomo.Operation do
-  @type t :: %__MODULE__{
-               method: Malomo.http_method_t(),
-               params: map,
-               path: String.t()
-             }
-
-  defstruct [method: nil, params: %{}, path: nil]
-end
