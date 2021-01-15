d9c41f1648eea758e3f5df03193b0b7c5b90e11d
Anthony Smith <420061+anthonator@users.noreply.github.com>
2021-01-15 17:00:41 -0500

    Added support for OAuth2 endpoints

---
+++ lib/malomo/auth.ex
@@ -0,0 +1,16 @@
+defmodule Malomo.Auth do
+  alias Malomo.{ Operation }
+
+  @doc """
+  Create an access code.
+  """
+  @spec create_access_token(Keyword.t()) :: Operation.t()
+  def create_access_token(opts) do
+    opts = Keyword.put(opts, :grant_type, "authorization_code")
+
+    %Operation{}
+    |> Map.put(:method, :post)
+    |> Map.put(:params, opts)
+    |> Map.put(:path, "/oauth2/auth")
+  end
+end
---
+++ test/malomo/auth_test.exs
@@ -0,0 +1,14 @@
+defmodule Malomo.AuthTest do
+  use ExUnit.Case, async: true
+
+  alias Malomo.{ Auth, Operation }
+
+  test "create_access_token/1" do
+    expected = %Operation{}
+    expected = Map.put(expected, :method, :post)
+    expected = Map.put(expected, :params, [grant_type: "authorization_code", p1: "v"])
+    expected = Map.put(expected, :path, "/oauth2/auth")
+
+    assert expected == Auth.create_access_token(p1: "v")
+  end
+end
