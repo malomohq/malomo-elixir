324a4da85a2afe954ec224cb006b3d00c615a918
Dorian Karter <dkarter@users.noreply.github.com>
2022-10-12 10:13:39 -0500

    feat: add additional return endpoints

--- lib/malomo/return.ex
+++ lib/malomo/return.ex
@@ -2,7 +2,7 @@ defmodule Malomo.Return do
   alias Malomo.Operation
 
   @doc """
-  Create a return.
+  Create a Return.
   """
   @spec create(Keyword.t()) :: Operation.t()
   def create(opts) do
@@ -10,5 +10,37 @@ defmodule Malomo.Return do
     |> Map.put(:method, :post)
     |> Map.put(:params, opts)
     |> Map.put(:path, "/returns")
+  end
+
+  @doc """
+  Retrieve a Return.
+  """
+  @spec get(binary()) :: Operation.t()
+  def get(id) do
+    %Operation{}
+    |> Map.put(:method, :get)
+    |> Map.put(:path, "/returns/#{id}")
+  end
+
+  @doc """
+  Retrieve a Return by a third-party alternate id.
+  """
+  @spec get_by_alternate_id(binary()) :: Operation.t()
+  def get_by_alternate_id(alternate_id) do
+    %Operation{}
+    |> Map.put(:method, :get)
+    |> Map.put(:params, [alternate_id: alternate_id])
+    |> Map.put(:path, "/returns")
+  end
+
+  @doc """
+  Update a Return.
+  """
+  @spec update(binary(), Keyword.t()) :: Operation.t()
+  def update(id, opts) do
+    %Operation{}
+    |> Map.put(:method, :put)
+    |> Map.put(:params, opts)
+    |> Map.put(:path, "/returns/#{id}")
   end
 end
